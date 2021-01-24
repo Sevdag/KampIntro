3043350b1118af6e7eec01814088dfed30cb6ed0
S_Gunes <gunesevdaa@gmail.com>
2021-01-24 21:06:47 +0200

    240121

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+        //Interface-Birbirinin alternatifi olan ama kod içerikleri farklı olan yapılar için kullanılır
+        void Hesapla();
+        void BiSeyYap();
+    }
+}
